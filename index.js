d21b3dee92dff1dfd2830177ec78ba5a1f37f657
Chris <orthagonal1@gmail.com>
2018-01-16 17:50:29 -0600

    update 1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const aug = require('aug');
-exports.register = (server, passedOptions, next) => {
+const register = (server, passedOptions) => {
   const cache = server.cache({
     segment: 'outputCache',
   });
@@ -8,86 +7,73 @@ exports.register = (server, passedOptions, next) => {
     ttl: 60 * 1000,
     key: (request) => request.url.href.replace(/.nocache=1/, '').replace(/.refreshcache=1/, '')
   };
-  const options = aug(defaultOptions, passedOptions);
+  const options = Object.assign({}, defaultOptions, passedOptions);
 
-  server.ext('onPreHandler', (request, reply) => {
+  server.ext('onPreHandler', async(request, h) => {
     if (!request.route.settings.plugins['hapi-output-cache'] ||
         request.auth.isAuthenticated ||
         request.query.nocache === '1' ||
         request.query.refreshcache === '1'
         ) {
-      return reply.continue();
+      return h.continue;
     }
     const key = options.key(request);
-
-    cache.get(key, (getErr, cached) => {
-      if (cached && cached.value) {
-        request.outputCache = true;
-        // go ahead and return the cached reply instead of continuing:
-        const response = reply(cached.value);
-        response.header('X-Output-Cache', 'hit');
-        response.header('X-Output-Cache-Updated', cached.updated);
-        response.header('X-Output-Cache-Expires', cached.expires);
-        return;
-      }
-      reply.continue();
-    });
+    const cached = await cache.get(key);
+    if (cached && cached.value) {
+      request.outputCache = true;
+      // go ahead and return the cached reply instead of continuing:
+      const response = h.response(cached.value);
+      response.header('X-Output-Cache', 'hit');
+      response.header('X-Output-Cache-Updated', cached.updated);
+      response.header('X-Output-Cache-Expires', cached.expires);
+      return response.takeover(cached.value);
+    }
+    return h.continue;
   });
 
-  const getCacheObj = function(request, response, ttl, done) {
+  const getCacheObj = async (request, response, ttl) => {
     const now = new Date().getTime();
     const cacheObj = {
       updated: new Date(),
       expires: new Date(now + ttl)
     };
-    if (response.variety === 'view') {
-      //render so we can store the output html in cache
-      request.render(response.source.template, response.source.context, response.source.options, (err, rendered, config) => {
-        if (err) {
-          return done(err);
-        }
-        cacheObj.value = rendered;
-        done(null, cacheObj);
-      });
-      return;
-    }
     cacheObj.value = response.source;
-    done(null, cacheObj);
+    return cacheObj;
   };
 
-  server.ext('onPreResponse', (request, reply) => {
+  server.ext('onPreResponse', async(request, h) => {
     const response = request.response;
     if (!request.route.settings.plugins['hapi-output-cache'] ||
         request.query.nocache === '1') {
       response.header('X-Output-Cache', 'disabled');
-      return reply.continue();
+      return h.continue;
     }
     if (request.outputCache) {
-      return reply.continue();
+      return h.continue;
     }
     if (response && response.output && response.output.statusCode !== 200) {
-      return reply.continue();
+      return h.continue;
     }
     const ttl = request.route.settings.plugins['hapi-output-cache'].ttl || options.ttl;
     const key = options.key(request);
-    getCacheObj(request, response, ttl, (err, cacheObj) => {
-      if (err) {
-        return server.log(['outputCache', 'renderError'], err);
-      }
-      cache.set(key, cacheObj, ttl, (setErr) => {
-        if (setErr) {
-          server.log(['outputCache', 'cacheError'], setErr);
-        } else {
-          server.log(['outputCache', 'set'], key);
-        }
-      });
-    });
-    request.response.header('X-Output-Cache', 'miss');
-    reply.continue();
+    try {
+      const cacheObj = await getCacheObj(request, response, ttl);
+      await cache.set(key, cacheObj, ttl);
+      server.log(['outputCache', 'set'], key);
+      // cache update header won't be automatically set as in previous version:
+      request.response.header('X-Output-Cache-Updated', cacheObj.updated.toString());
+      request.response.header('X-Output-Cache', 'miss');
+    } catch (err) {
+      server.log(['outputCache', 'error'], err);
+    }
+    return h.continue;
   });
-  next();
 };
 
-exports.register.attributes = {
+
+exports.plugin = {
+  name: 'hapi-output-cache',
+  register,
+  once: true,
   pkg: require('./package.json')
 };
