730d25fb484fb161aabe3c38a24e5efbc882fbe0
Chris <orthagonal1@gmail.com>
2018-01-17 19:03:35 -0600

    udpate example

--- example/index.js
+++ example/index.js
@@ -1,94 +1,89 @@
 const Hapi = require('hapi');
 const Boom = require('boom');
 
-const server = new Hapi.Server({
-  debug: {
-    log: ['outputCache']
-  }
-});
-server.connection({ port: 8080 });
-
-server.register([
-  { register: require('vision') },
-  {
-    register: require('../'),
-    options: {
-      ttl: 10 * 1000
+const f = async() => {
+  const server = new Hapi.Server({
+    debug: {
+      log: ['outputCache']
+    },
+    port: 8080
+  });
+  await server.register([
+    { plugin: require('vision') },
+    {
+      plugin: require('../'),
+      options: {
+        ttl: 10 * 1000
+      }
     }
-  }
-], (err) => {
-  if (err) {
-    throw err;
-  }
+  ]);
   server.views({
     path: `${__dirname}/views`,
     engines: {
       html: require('handlebars')
     }
   });
-});
-
-server.route([
-  {
-    method: 'GET',
-    path: '/',
-    config: {
-      plugins: {
-        'hapi-output-cache': {
-        }
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      config: {
+        plugins: {
+          'hapi-output-cache': {
+          }
+        },
       },
-    },
-    handler(request, reply) {
-      reply(new Date().getTime());
-    }
-  },
-  {
-    method: 'GET',
-    path: '/long',
-    config: {
-      plugins: {
-        'hapi-output-cache': {
-          ttl: 60 * 1000
-        }
-      },
-    },
-    handler(request, reply) {
-      reply(new Date().getTime());
-    }
-  },
-  {
-    method: 'GET',
-    path: '/view',
-    config: {
-      plugins: {
-        'hapi-output-cache': {
-          ttl: 5 * 1000
-        }
-      },
-    },
-    handler(request, reply) {
-      reply.view('homepage', {
-        date: new Date().getTime()
-      });
-    }
-  },
-  {
-    method: 'GET',
-    path: '/error',
-    config: {
-      plugins: {
-        'hapi-output-cache': {
-          ttl: 5 * 1000
-        }
+      handler(request, h) {
+        return new Date().getTime();
       }
     },
-    handler(request, reply) {
-      reply(Boom.badRequest('error'));
+    {
+      method: 'GET',
+      path: '/long',
+      config: {
+        plugins: {
+          'hapi-output-cache': {
+            ttl: 60 * 1000
+          }
+        },
+      },
+      handler(request, h) {
+        return new Date().getTime();
+      }
+    },
+    {
+      method: 'GET',
+      path: '/view',
+      config: {
+        plugins: {
+          'hapi-output-cache': {
+            ttl: 5 * 1000
+          }
+        },
+      },
+      handler(request, h) {
+        return h.view('homepage', {
+          date: new Date().getTime()
+        });
+      }
+    },
+    {
+      method: 'GET',
+      path: '/error',
+      config: {
+        plugins: {
+          'hapi-output-cache': {
+            ttl: 5 * 1000
+          }
+        }
+      },
+      handler(request, h) {
+        throw Boom.badRequest('error');
+      }
     }
-  }
-]);
-
-server.start(() => {
+  ]);
+  await server.start();
   console.log('Server running at:', server.info.uri);
-});
+};
 
+f();
